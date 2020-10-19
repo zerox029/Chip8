70eaac26bfc27b9b9e7824b7c056c4a6ac0638c0
Étienne Plante <zerox029@gmail.com>
2020-10-19 14:41:21 -0400

    :bug: Fixed stack size

--- src/chip8/Utils.java
+++ src/chip8/Utils.java
@@ -2,7 +2,7 @@ package chip8;
 
 public class Utils {
     public final static int MEMORY_SIZE = 4096;
-    public final static int STACK_SIZE = 4096;
+    public final static int STACK_SIZE = 16;
 
     public final static short FIRST_PROGRAM_SPACE_ADDRESS = 0x200;
     public final static short MAX_MEMORY_ADDRESS = 0xFFF;

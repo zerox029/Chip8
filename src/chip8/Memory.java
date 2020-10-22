30780fc05fe4299e707151707ac0caa9f61e1e78
Étienne Plante <zerox029@gmail.com>
2020-10-22 09:07:09 -0400

    :sparkles: Added hexadecimal sprites

--- src/chip8/Memory.java
+++ src/chip8/Memory.java
@@ -4,10 +4,14 @@ public class Memory {
     private byte[] memory;
     private short[] stack;
 
+    private boolean[][] screenMemory;
+
     public Memory()
     {
         memory = new byte[Utils.MEMORY_SIZE];
         stack = new short[Utils.STACK_SIZE];
+
+        loadHexSpritesToMemory();
     }
 
     public byte getMemoryAtAddress(short address)
@@ -40,6 +44,74 @@ public class Memory {
         if(pointer < Utils.STACK_SIZE)
         {
             stack[pointer] = value;
+        }
+    }
+
+    private void loadHexSpritesToMemory()
+    {
+        for(byte i = 0; i < Utils.sprite_0.length; i++)//0
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + i), Utils.sprite_0[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_1.length; i++)//1
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 5 + i), Utils.sprite_1[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_2.length; i++)//2
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 10 + i), Utils.sprite_2[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_3.length; i++)//3
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 15 + i), Utils.sprite_3[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_4.length; i++)//4
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 20 + i), Utils.sprite_4[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_5.length; i++)//5
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 25 + i), Utils.sprite_5[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_6.length; i++)//6
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 30 + i), Utils.sprite_6[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_7.length; i++)//7
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 35 + i), Utils.sprite_7[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_8.length; i++)//8
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 40 + i), Utils.sprite_8[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_9.length; i++)//9
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 45 + i), Utils.sprite_9[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_a.length; i++)//A
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 50 + i), Utils.sprite_a[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_b.length; i++)//B
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 55 + i), Utils.sprite_b[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_c.length; i++)//C
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 60 + i), Utils.sprite_c[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_d.length; i++)//D
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 65 + i), Utils.sprite_d[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_e.length; i++)//E
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 70 + i), Utils.sprite_e[i]);
+        }
+        for(byte i = 0; i < Utils.sprite_e.length; i++)//F
+        {
+            setMemoryAtAddress((short) (Utils.spritesStorageStartingAddress + 75 + i), Utils.sprite_f[i]);
         }
     }
 }
--- src/chip8/Utils.java
+++ src/chip8/Utils.java
@@ -9,5 +9,27 @@ public class Utils {
 
     public static final String ROMS_PATH = "roms/";
 
+    public static final short spritesStorageStartingAddress = 0x000;
+
+    //Number sprites
+    public static final byte[] sprite_0 = new byte[]{(byte)0xF0,(byte)0x90,(byte)0x90,(byte)0x90,(byte)0xF0};
+    public static final byte[] sprite_1 = new byte[]{(byte)0x20,(byte)0x60,(byte)0x20,(byte)0x20,(byte)0x70};
+    public static final byte[] sprite_2 = new byte[]{(byte)0xF0,(byte)0x10,(byte)0xF0,(byte)0x80,(byte)0xF0};
+    public static final byte[] sprite_3 = new byte[]{(byte)0xF0,(byte)0x10,(byte)0xF0,(byte)0x10,(byte)0xF0};
+    public static final byte[] sprite_4 = new byte[]{(byte)0x90,(byte)0x90,(byte)0xF0,(byte)0x10,(byte)0x10};
+    public static final byte[] sprite_5 = new byte[]{(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x10,(byte)0xF0};
+    public static final byte[] sprite_6 = new byte[]{(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x90,(byte)0xF0};
+    public static final byte[] sprite_7 = new byte[]{(byte)0xF0,(byte)0x10,(byte)0x20,(byte)0x40,(byte)0x40};
+    public static final byte[] sprite_8 = new byte[]{(byte)0xF0,(byte)0x90,(byte)0xF0,(byte)0x90,(byte)0xF0};
+    public static final byte[] sprite_9 = new byte[]{(byte)0xF0,(byte)0x90,(byte)0xF0,(byte)0x10,(byte)0xF0};
+
+    //Letter sprites from A to F
+    public static final byte[] sprite_a = new byte[]{(byte)0xF0,(byte)0x90,(byte)0xF0,(byte)0x90,(byte)0x90};
+    public static final byte[] sprite_b = new byte[]{(byte)0xE0,(byte)0x90,(byte)0xE0,(byte)0x90,(byte)0xE0};
+    public static final byte[] sprite_c = new byte[]{(byte)0xF0,(byte)0x80,(byte)0x80,(byte)0x80,(byte)0xF0};
+    public static final byte[] sprite_d = new byte[]{(byte)0xE0,(byte)0x90,(byte)0x90,(byte)0x90,(byte)0xE0};
+    public static final byte[] sprite_e = new byte[]{(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x80,(byte)0xF0};
+    public static final byte[] sprite_f = new byte[]{(byte)0xF0,(byte)0x80,(byte)0xF0,(byte)0x80,(byte)0x80};
+
     private Utils(){}; //Private constructor to hide the public one
 }
